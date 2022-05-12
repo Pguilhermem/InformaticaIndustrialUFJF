2ce2b560b9117aed1e97d106b371106120f1a261
Sukita25 <105301105+Sukita25@users.noreply.github.com>
2022-05-12 14:21:24 -0300

    Update main.cpp

--- C++/Aula1/calculadora/main.cpp
+++ C++/Aula1/calculadora/main.cpp
@@ -1,20 +1,22 @@
-#include <iostream>
+//Calculadora
 
-using namespace std;
+#include <iostream> //inclui biblioteca de entrada/saída de dados
 
-int main()
+using namespace std;//inclusão do espaço de nomes std
+
+int main()//a função main inicia a execução do programa
 {
-	float num1,num2;
-	char op;
+	float num1,num2; //cria os numeros que serão operados
+	char op; // cria a variavel que recebera os operadores
 
-	cout << "Num 1: ";
-	cin >> num1;
+	cout << "Num 1: "; 
+	cin >> num1; //recebe o numero 1
 	cout << "Num 2: ";
-	cin >> num2;
+	cin >> num2; // recebe o numero 2
 	cout << "op: ";
-	cin >> op;
+	cin >> op; // recebe o operaor
 
-	switch(op)
+	switch(op)// verifica o operador escolhido e escolhe o casi baseado nele
 	{
 	case '+':
 		cout << "Resultado = " <<num1+num2<<endl;
@@ -28,7 +30,7 @@ int main()
 	case '/':
 		cout << "Resultado = " << num1/num2<<endl;
 		break;
-	default:
+	default:// esse caso acontece caso o Switch não consiga indentifica um case adequado
 		cout << "Operacao invalida";
 		break;
 	}
