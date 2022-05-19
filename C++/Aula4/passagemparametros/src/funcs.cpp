ed5527a91f9cf671c6e704ba554a80376d78f52b
Sukita25 <105301105+Sukita25@users.noreply.github.com>
2022-05-19 13:18:13 -0300

    Update funcs.cpp

--- C++/Aula4/passagemparametros/src/funcs.cpp
+++ C++/Aula4/passagemparametros/src/funcs.cpp
@@ -1,15 +1,15 @@
-void passagemValor(int x)
+void passagemValor(int x) //É criada uma copia do dado com o nome "x" e é feiata a operação na copia, sem alterar a variavel do codigo principal
 {
-    x++;
+    x++; //Não altera a variavel do codigo principal
 }
 
-void passagemPonteiro(int *x)
+void passagemPonteiro(int *x) //É criado um ponteiro com o nome "x" que aponta para a o local onde a variavel do codigo principal está, sendo possivel altera a mesma
 {
-    (*x)++;
+    (*x)++; //Altera a variavel do codigo principal
 }
 
-void passagemReferencia(int &x)
+void passagemReferencia(int &x)//O nome "x" passa a se referir a variavel do codigo principal, todas as alterações feitas nela dentro da função alteram a mesma no codigo principal
 {
-    x++;
+    x++;//Altera a variavel do codigo principal
 }
 
