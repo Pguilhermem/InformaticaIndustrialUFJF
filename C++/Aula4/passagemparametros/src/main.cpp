1a8be8da3269b4128e57d526cde88196db2d37b5
Sukita25 <105301105+Sukita25@users.noreply.github.com>
2022-05-19 13:20:46 -0300

    Update main.cpp

--- C++/Aula4/passagemparametros/src/main.cpp
+++ C++/Aula4/passagemparametros/src/main.cpp
@@ -8,17 +8,17 @@ int main()
 	int var = 10;
 
 	//Passagem por valor
-	cout<<"Valor de var: "<<var<<endl;	
-	passagemValor(var);
-	cout<<"Valor de var: "<<var<<endl;
+	cout<<"Valor de var: "<<var<<endl;//Imprime 10	
+	passagemValor(var);//Não altera a variavel
+	cout<<"Valor de var: "<<var<<endl; //Imprime 10
 	
 	//Passagem por ponteiro
-	cout << "Valor de var: " << var << endl;
-	passagemPonteiro(&var);
-	cout << "Valor de var: " << var << endl;
+	cout << "Valor de var: " << var << endl;//Imprime 10
+	passagemPonteiro(&var);//Altera a variavel
+	cout << "Valor de var: " << var << endl;//Imprime 11
 
 	//Passagem por referência
-	cout << "Valor de var: " << var << endl;
-	passagemReferencia(var);
-	cout << "Valor de var: " << var << endl;
+	cout << "Valor de var: " << var << endl;//Imprime 10
+	passagemReferencia(var);//Altera a variavel
+	cout << "Valor de var: " << var << endl;//Imprime 11
 }
