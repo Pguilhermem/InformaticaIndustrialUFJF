931ce22600b646257db46c4398f8ed827b01a45b
Sukita25 <105301105+Sukita25@users.noreply.github.com>
2022-05-19 13:44:41 -0300

    Update banco.h

--- C++/Aula6/SistemaBancario/inc/banco.h
+++ C++/Aula6/SistemaBancario/inc/banco.h
@@ -3,16 +3,16 @@
 
 #include "conta.h"
 
-#define NUMCONTAS 100
+#define NUMCONTAS 100 //Define que a palavar NUMCONTAS passa a valer como escrever 100
 
 class Banco
 {
 private:
-    Conta contas[NUMCONTAS];
+    Conta contas[NUMCONTAS];//Cria um vetor de objetos do tipo Conta que pode armazenar até 100 contas
 public:
     Banco();
     ~Banco();
-    Conta* buscaConta(int numero);
+    Conta* buscaConta(int numero); //Metodo que retorna o endereço do objeto conta que possui o mesmo numero informado
     void atendimento();
 };
 
