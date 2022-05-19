692dba85e94f08648ab83416d89c9cbf69e9f7b3
Sukita25 <105301105+Sukita25@users.noreply.github.com>
2022-05-19 13:33:53 -0300

    Update main.cpp

--- C++/Aula5/classes/src/main.cpp
+++ C++/Aula5/classes/src/main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 {
-	Conta c1;
-	c1.numero = 2345;
+	Conta c1;//Cria o objeto c1 que pertence a classe conta
+	c1.numero = 2345;//Altera o numero do objeto c1
 	c1.titular = "Joao";
-	c1.exibeDados();
+	c1.exibeDados();//Usa o metodo exibeDados() da classe Conta para o objeto c1
 
 }
