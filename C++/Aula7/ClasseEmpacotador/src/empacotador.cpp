ff31b247798d2ad319e150dcd123fd8ffec88ffa
Sukita25 <105301105+Sukita25@users.noreply.github.com>
2022-05-19 14:56:00 -0300

    Update empacotador.cpp

--- C++/Aula7/ClasseEmpacotador/src/empacotador.cpp
+++ C++/Aula7/ClasseEmpacotador/src/empacotador.cpp
@@ -3,34 +3,34 @@
 
 using namespace std;
 
-int Empacotador::totalEmpacotados = 0 ;
+int Empacotador::totalEmpacotados = 0 ;//Essa variavel é compartilhada com todas as classe empacotadores
 
-Empacotador::Empacotador(int id): id(id),numEmpacotados(0)
+Empacotador::Empacotador(int id): id(id),numEmpacotados(0)//o atributo id recebe id, e o numEmpacotados recebe 0
 {
 }
 
-void Empacotador::empacotar(int numPacotes)
+void Empacotador::empacotar(int numPacotes)//Incrementa a as variaveis total numEmpacotados e totalEmpacotados
 {
     this->numEmpacotados+=numPacotes;
     Empacotador::totalEmpacotados += numPacotes;
 }
 
-int Empacotador::getNumEmpacotados()
+int Empacotador::getNumEmpacotados()//Retorna o numero de pacotes empacotados por esse objeto empacotador
 {
     return this->numEmpacotados;
 }
 
-int Empacotador::getTotalEmpacotados()
+int Empacotador::getTotalEmpacotados()//Retorna o total de empacotados
 {
     return Empacotador::totalEmpacotados;
 }
 
-void Empacotador::imprimeNumEmpacotados()
+void Empacotador::imprimeNumEmpacotados()//Imprime o numero de pacotes empacotados por esse empacotador
 {
     cout<<"Numero de pacotes empacotados pelo empacotador "<<this->id<<" : "<<this->getNumEmpacotados()<<endl;
 }
 
-void Empacotador::imprimeTotalEmpacotados()
+void Empacotador::imprimeTotalEmpacotados()//Imprime o total que foi empacotado e quantas vezes ouve a execução do metodo
 {
     static int numchamadas=0;
     cout<<"Numero total de pacotes empacotados :"<<Empacotador::totalEmpacotados<<endl;
