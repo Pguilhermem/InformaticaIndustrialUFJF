90db3b6b7be931da2ecb25af8b857a3c7d6877a7
Sukita25 <105301105+Sukita25@users.noreply.github.com>
2022-05-20 12:20:07 -0300

    Update horario.cpp

--- C++/Aula7/Horario/src/horario.cpp
+++ C++/Aula7/Horario/src/horario.cpp
@@ -8,48 +8,48 @@ Horario::Horario(int h , int m , int s)
 :var(10)
 {
     var = 10;
-    setHorario(h,m,s);
+    setHorario(h,m,s);//Define os horarios recebidos no construtor
 }
 
-void Horario::setHora(int h) 
+void Horario::setHora(int h) //Define a hora se ela estiver dentro da condição h>=0 && h<24
 {
-    this->hora = (h>=0 && h<24) ? h : 0;
+    this->hora = (h>=0 && h<24) ? h : 0;//Essa linha diz basicamente, se h>=0 && h<24 faça this->hora = h, se não faça h=0
 }
 
-void Horario::setMinuto(int m)
+void Horario::setMinuto(int m)//Define a hora se ela estiver dentro da condição m>=0 && m<60
 {
-    this->minuto = (m>=0 && m<60) ? m : 0;
+    this->minuto = (m>=0 && m<60) ? m : 0;//Essa linha diz basicamente, se m>=0 && m<60 faça this->hora = m, se não faça m=0
 }
 
-void Horario::setSegundo(int s)
+void Horario::setSegundo(int s)//Define a hora se ela estiver dentro da condição s>=0 && s<60
 {
-    this->segundo = (s>=0 && s<60) ? s : 0;
+    this->segundo = (s>=0 && s<60) ? s : 0;//Essa linha diz basicamente, se s>=0 && s<60 faça this->hora = s, se não faça s=0
 }
 
-void Horario::setHorario(int h, int m, int s)
+void Horario::setHorario(int h, int m, int s)//Define os horarios recebidos no construtor
 {
     setHora(h);
     setMinuto(m);
     setSegundo(s);
 }
 
-int Horario::getHora() const 
+int Horario::getHora() const //retorna a hora
 {
     
     return this->hora;
 }
 
-int Horario::getMinuto() const 
+int Horario::getMinuto() const //retorna o minuto
 {
     return this->minuto;
 }
 
-int Horario::getSegundo() const
+int Horario::getSegundo() const//retorna o segundo
 {
     return this->segundo;
 }
 
-void Horario::imprimeHorario() const 
+void Horario::imprimeHorario() const //retorna a hora no formato HH:MM:SS
 {
     cout<<setfill('0')<<setw(2)<<this->getHora()<<":"<<setw(2)<<this->getMinuto()<<":"<<setw(2)<<this->getSegundo()<<endl;
 }
