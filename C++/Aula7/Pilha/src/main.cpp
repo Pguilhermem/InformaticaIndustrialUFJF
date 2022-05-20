4ff872888020f2cb5ba69c1ac191e541bdda7e0a
Sukita25 <105301105+Sukita25@users.noreply.github.com>
2022-05-20 12:40:50 -0300

    Update main.cpp

--- C++/Aula7/Pilha/src/main.cpp
+++ C++/Aula7/Pilha/src/main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 template<typename T>
-void imprimeVetor(T* p, int tam)
+void imprimeVetor(T* p, int tam) //Essa função pode receber qualquer tipo de vetor para imprimir
 {
     for(int i=0; i<tam;i++)
     {
@@ -20,12 +20,12 @@ int main()
     cout << "Digite o tamanho do vetor:";
     cin>>tamVet;
 
-    int *pvet = new int[tamVet];
+    int *pvet = new int[tamVet];//Cria um vetor de inteiros de tamanho tamVet e faz pvet apontar para a primeira posição desse
 
     cout << "--- Vetor Alocado Dinamicamente ---"<<endl;
     for(int i=0;i<tamVet;++i)
     {
-        *(pvet+i) = i;
+        *(pvet+i) = i; //Atribui o valor 0 para posição 0, 1 para a 1 e assim por diante
         cout << "pvet["<<i<<"] = " << pvet[i]<<endl;
     }    
     delete[] pvet;
@@ -57,15 +57,15 @@ int main()
     p1.remove(outInt);
     pString.remove(outStr);
 
-    cout<<"Dado removido da pilha de inteiros: "<<outInt<<endl;
-    cout<<"Dado removido da pilha de strings: "<<outStr<<endl;
+    cout<<"Dado removido da pilha de inteiros: "<<outInt<<endl; //Removeu o 10
+    cout<<"Dado removido da pilha de strings: "<<outStr<<endl;//Removeu o "Hello World"
 
     cout << "--- Segunda Remocao ---"<<endl;
     p1.remove(outInt);
     pString.remove(outStr);
 
-    cout<<"Dado removido da pilha de inteiros: "<<outInt<<endl;
-    cout<<"Dado removido da pilha de strings: "<<outStr<<endl;
+    cout<<"Dado removido da pilha de inteiros: "<<outInt<<endl; //Removeu o 4
+    cout<<"Dado removido da pilha de strings: "<<outStr<<endl;//Removeu o "Ola Mundo"
 
     return 0;
 }
