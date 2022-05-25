692cfe6ec82b12cfd9dafde5fe820be3fc20ec5a
Sukita25 <105301105+Sukita25@users.noreply.github.com>
2022-05-25 17:11:03 -0300

    Update contacorrente.h

--- C++/Aula8/ExemploHerancaConta/inc/contacorrente.h
+++ C++/Aula8/ExemploHerancaConta/inc/contacorrente.h
@@ -2,13 +2,13 @@
 #define CONTA_CORRENTE_H
 #include "conta.h"
 
-class ContaCorrente : public Conta
+class ContaCorrente : public Conta //ContaCorrente deriva de Conta, E todos os atributos publicos da Conta continuam publicos para Contacorrente assim como os privados e protegidos
 {
-private:
+private://Todos os atributos privados de Conta existem implicitamente aqui tambem, mas o contrario não e verdade
     long long numCartaoCredito;
     double totalFatura;
-public:
-    ContaCorrente();
+public://Todos os atributos publicos de Conta existem implicitamente aqui tambem, mas o contrario não e verdade
+    ContaCorrente();//ContaCorrente possui um construtor diferente de Conta
     ContaCorrente(int senha, int numero, std::string titular, double saldo, long long numCartaoCredito, double totalFatura);
     ~ContaCorrente();    
     bool exibeDados(int senha);
