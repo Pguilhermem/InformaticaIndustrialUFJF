dec9f5923e85f0d451138e3f8da7c80407558198
Sukita25 <105301105+Sukita25@users.noreply.github.com>
2022-05-25 17:17:02 -0300

    Update contapoupanca.h

--- C++/Aula8/ExemploHerancaConta/inc/contapoupanca.h
+++ C++/Aula8/ExemploHerancaConta/inc/contapoupanca.h
@@ -2,12 +2,12 @@
 #define CONTA_POUPANCA_H
 #include "conta.h"
 
-class ContaPoupanca : public Conta
+class ContaPoupanca : public Conta//ContaPoupanca deriva de Conta, E todos os atributos publicos da Conta continuam publicos para Contacorrente assim como os privados e protegidos
 {
-private:
+private://Todos os atributos privados de Conta existem implicitamente aqui tambem, mas o contrario não e verdade
     float taxa;
-public:
-    ContaPoupanca();
+public://Todos os atributos publicos de Conta existem implicitamente aqui tambem, mas o contrario não e verdade
+    ContaPoupanca();//ContaPoupanca possui um construtor diferente de Conta
     ContaPoupanca(int senha, int numero, std::string titular, double saldo, float taxa = 0);
     ~ContaPoupanca();
     double simulaRendimentos(int senha, int numMeses);
