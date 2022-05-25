75e6301e907df39b4c25cab41068508050905eae
Sukita25 <105301105+Sukita25@users.noreply.github.com>
2022-05-25 17:39:19 -0300

    Update sensorindustrial.h

--- C++/Aula8/Polimorfismo/inc/sensorindustrial.h
+++ C++/Aula8/Polimorfismo/inc/sensorindustrial.h
@@ -21,14 +21,14 @@ public:
     //funções set
     void setId(const string&);
     void setUnidade(const string&);
-    void lerDados(const vector<Medicao>& dadosNovos);
+    void lerDados(const vector<Medicao>& dadosNovos);//Função alvo de polimorfismo
 
     //funções get
     string getId();
     string getUnidade();
     int getNumMed();
     bool getDado(const string&, Medicao&);
-    virtual void imprimeDados();
-};
+    virtual void imprimeDados();//Função alvo de polimorfismo,Apesar de Sensor com ajuste possuir um Metodo imprimeDados(), um objeto da classe SensorComAjuste ainda sim
+};                              //pode acessar esse Metodo da classe "Pai" ao inves de sua propria versão, oque não ocorre com o metodo lerDados(Cada um tem o seu) 
 
 #endif
