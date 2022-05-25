6c900dc51055b04b296ebf8c5ed1b7277593ec0a
Sukita25 <105301105+Sukita25@users.noreply.github.com>
2022-05-25 17:40:07 -0300

    Update sensorcomajuste.h

--- C++/Aula8/Polimorfismo/inc/sensorcomajuste.h
+++ C++/Aula8/Polimorfismo/inc/sensorcomajuste.h
@@ -18,8 +18,9 @@ public:
     void setCoef(vector<double>&);
     void getCoef(vector<double>&);
 
-    void lerDados(const vector<Medicao>&);
-    void imprimeDados();
+    void lerDados(const vector<Medicao>&);//Função alvo de polimorfismo
+    void imprimeDados();//Função alvo de polimorfismo,Apesar de Sensor com ajuste possuir um Metodo imprimeDados(), um objeto da classe SensorComAjuste ainda sim
+                        //pode acessar esse Metodo da classe "Pai" ao inves de sua propria versão, oque não ocorre com o metodo lerDados(Cada um tem o seu) 
 
     
 };
