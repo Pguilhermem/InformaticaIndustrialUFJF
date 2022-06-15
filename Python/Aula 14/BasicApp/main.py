b1f5178e5d9c5a9a265bedd6a82836c96a7e2953
Guilherme Soares <guilherme.marcio@engenharia.ufjf.br>
2022-06-15 16:56:49 -0300

    add Aula 14

---
+++ Python/Aula 14/BasicApp/main.py
@@ -0,0 +1,22 @@
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.config import Config
+
+class MyWidget(BoxLayout):
+    def changelb(self):
+        """
+        Método simples para incremento do valor mostrado no label
+        """
+        self.ids.lb.text = str(int(self.ids.lb.text) + 1) 
+
+class BasicApp(App):
+    def build(self):
+        """
+        Método para construção do aplicativo com base no widget criado
+        """
+        return MyWidget()
+ 
+if __name__ == '__main__':
+    Config.set('graphics','resizable',True)
+    BasicApp().run()
---
+++ Python/Aula 14/KivyBasico/main.py
@@ -0,0 +1,34 @@
+import kivy
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+
+class BasicApp(App):
+    """
+    Aplicativo básico Kivy
+    """
+    def build(self):
+        layout = BoxLayout(orientation = 'horizontal')
+        self.lb = Label(text="0")
+        bt = Button(text = "Botão 1",on_release=self.incrementar)
+        layout.add_widget(self.lb)
+        layout.add_widget(bt)
+        
+        
+        layout2 = BoxLayout(orientation = 'vertical')
+        self.lb2 = Label(text="0", color=[1,0,0,1])
+        self.lb3 = Label(text="0")
+        layout2.add_widget(self.lb2)
+        layout2.add_widget(self.lb3)
+        layout.add_widget(layout2)
+
+        return layout
+    
+    def incrementar(self,*args):
+        self.lb.text = str(int(self.lb.text)+1) 
+        self.lb2.text = str(int(self.lb2.text)+2) 
+        self.lb3.text = str(int(self.lb3.text)+3) 
+
+if __name__=='__main__':
+    BasicApp().run()
