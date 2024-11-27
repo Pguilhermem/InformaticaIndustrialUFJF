71cd52bd9680bc11699a66933cd41b689fff1f45
Pguilhermem <39747038+Pguilhermem@users.noreply.github.com>
2024-11-27 16:11:51 -0300

    Update conta.cpp

--- C++/Aula4/classes/src/conta.cpp
+++ C++/Aula4/classes/src/conta.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-void Conta::exibeDados(int numero)
+void Conta::exibeDados()
 {
 	cout << "Titular: " << this->titular << endl;
 	cout << "Numero: " << this->numero << endl;
