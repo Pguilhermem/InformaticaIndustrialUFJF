460e3d610dd2215c7d10c1d213a05b2339453ed5
denis magno <denismagnocarvalho@gmail.com>
2025-10-09 11:09:09 -0300

    teste

--- C++/Aula5/ExemploClasseConta/src/conta.cpp
+++ C++/Aula5/ExemploClasseConta/src/conta.cpp
@@ -53,13 +53,7 @@ double Conta::getSaldo(int senha) //Metodo que mediante a inserção de uma senh
 
 }
 
-void Conta::setSaldo(double valor)//Metodo que define um saldo, atravez desse metodo é possivel ter um saldo negativo
-{
-    if(valor>0)
-        this->saldo = valor;
-    else
-        std::cout<<"Valor inválido"<<std::endl;
-}
+
 
 void Conta::setSenha(int novaSenha)//Metodo que define o atributo senha
 {
