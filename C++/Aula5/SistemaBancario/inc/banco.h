18acc3157e7e97b28b412c4ffe7758cf07509ba6
dMagn0 <denismagnocarvalho@gmail.com>
2025-11-23 20:14:27 -0300

    Aula 5 - SistemaBancario modelado

--- C++/Aula5/SistemaBancario/inc/banco.h
+++ C++/Aula5/SistemaBancario/inc/banco.h
@@ -14,6 +14,7 @@ public:
     ~Banco();
     Conta* buscaConta(int numero); //Metodo que retorna o endereço do objeto conta que possui o mesmo numero informado
     void atendimento();
+    void imprimeTerminal(int resultadoOperacao);
 };
 
 
